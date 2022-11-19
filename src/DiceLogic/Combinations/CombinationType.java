f24d5edc7ce0d450c7aa6894f662e71be58f74d9
chechmek <romanklut89@gmail.com>
2022-11-19 13:32:32 +0100

    made combination interface

---
+++ src/DiceLogic/Combinations/CombinationType.java
@@ -0,0 +1,11 @@
+package DiceLogic.Combinations;
+
+public enum CombinationType {
+    Triplet2,
+    Triplet3,
+    Triplet4,
+    Triplet5,
+    Triplet6,
+    Single1,
+    Singel5
+}
---
+++ src/DiceLogic/Combinations/Triplet.java
@@ -0,0 +1,22 @@
+package DiceLogic.Combinations;
+
+import DiceLogic.Die;
+
+import java.util.ArrayList;
+
+public class Triplet implements Combination{
+    @Override
+    public int getPoints() {
+        return 0;
+    }
+
+    @Override
+    public CombinationType getType() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Die> getDice() {
+        return null;
+    }
+}
